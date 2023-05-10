e328f5ed7cb671d0a87e43e1127da731392b71f6
vaarunmuthappan <vaarunmuthappan@gmail.com>
2023-05-09 21:23:37 -0700

    environment removed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (proccess.env.NODE_ENV === 'production') {
-  disableReactDevTools();
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
