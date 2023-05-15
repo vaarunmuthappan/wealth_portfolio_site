cc05fd9b91c03f316d6f4ec87a112808a37b3cfb
vaarunmuthappan <vaarunmuthappan@gmail.com>
2023-05-14 18:02:48 -0700

    slash for routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Login />} />
-        <Route path="register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
 
 
         <Route path="dash" element={<DashLayout />}>
